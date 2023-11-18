387c56cd45b250a8a2f8d3d6409da9f17b3486a4
ericnjeru <ericmukundi10@gmail.com>
2023-11-18 19:49:25 +0300

    chore: export funcs

--- functions/export.js
+++ functions/export.js
@@ -1,9 +1,19 @@
 const admin = require('firebase-admin')
 const fs = require('fs')
 
-// Initialize Firebase Admin SDK
-admin.initializeApp()
-
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: 'civicbase-functions',
+    privateKey:
+      '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDGKh+fKWKdmDBl\nI6unEzdgpJ15hE7H4JTJDONT+/RCiltyCmjXpgiSZslhYvFW95vNsom/uSKnlSvJ\nxmmXyAHFo9eeAITJ0R5EdYVyaRZsWv439mb4dd/4ZMNGxygiKkslCBOwjZdCix52\nE48FiiGl1dnyp5d+WO3QTDc4buLwHzbP5F7EKPNp37nRGTCft3anviOhCxzRVUrG\np6L/VuDeGLjKkR0asWJZptrjdMSBczX28rWOQNQ/wjbpHZDFWAkSqNjB2pJVIEVe\n4X7H9UkD5Rs5vrqv8cFyeCfAlCcmj2O+LeB1VHzdfcL5ST+clm6leD++SlvuPji0\nDGqK5eMFAgMBAAECggEAEDHBec4pBMk6+aXFN/2TCS8KvrNR7A9UOWxeaGIIksBT\nbtA49zHWm36sHTnq9sXiq36LhINXUkV98zRnmeoHcwsyHY1CgASHkH95YC5qxBol\n+Mhs0js+PXXTaF8lAX3EiSQBPLlyT61AC26kjoa8vALsMWKbnjPoSdCHGfG97AxY\n9a3sdbtUzLUHY0bSALZ3xyLtkWhMr4wq2mxpn4Y7nm4UM2Iq7mQtGm66p4PZJaSF\n9nHik3VY2/xzzwDRntw9PzkPOiPiX7NCQpoRfpa57xlhoWL90dHTik6QaWV1aMa+\nqs6yTW6UVgJcwcl0qzTnFyYxAZwSW2gVSOSRr5xRtQKBgQDnBwUfcBQeIy95Dbch\ncr1tNxRCdtHNQvaIwigxaY8HK3F7TICyQl7tFkfXUIUFD+HdQZivY5DZMwtEQWa7\nBlfXGE2ZLxLvOGFLnfKVbGcggO96PFRoJ1Br0h67sEJDiMbpPq7DH8t8+mpofIPm\nslPGzdg/HTY4o3Ozxom9FaAuCwKBgQDblbk4v6rAh5kCPM9yjWIZidkx+faPbpdQ\nLkVMl4l3X3+CLRXNtrR4LbApj+4LEPkPTP7QT1fVlplNq7CSeuT2x/i5LsUqUPgg\nJILEbPjeHdTGg2wtOypgB9Ge2NAO5D2S1G+8vvwP3xWhWyiEbZCLLgMq6+E74EPd\nuD229iCtLwKBgDnj0m7MQN5EZtLokuj4vWqukI8PAwdyLiCatkObddGQG0GQK/d/\nFhZGrP3RGWPpVOmCVGYuhs9GMR6LXNikgEJPvrIRz6bN8+Nk0rKKlVTZuihhEa1/\nnHFIF5dkVVZi4aT0pgoS34STozJHcS3PpZp9XiCCi2Gp44Y6y6zrzFV9AoGBALS3\n772tuOlFb/8wbwb3J0Kfedqwv2RifzPlMPGNUfyl4voPAv/QG41BBNff9eizqlcW\nZiyo8D//9uG+qM6VSDV2dNRrkqd2Bg/mHJ1ulg+v+3r5lKFRLVnldXbHN8zZuXdL\nUT7E7Axf7Jxkut7qkWiKkN7VTQSLWS90P46B2HjPAoGBAMxbJpK9EYRaDg02Yjx+\nDBjEUO+VbPaQRACZZWuqmwHNGJcKis5mFHWSbNH5QMAq6b7c19WNw2r1u6HNLkA6\nRjRa7koYAp4j1wM2iezH5gJuau+BML8XAuDwDq8Qk7DVfRMEg6mRbFoxFIQaw+8F\nArJr8o+dojTr7fx6aCQueXym\n-----END PRIVATE KEY-----\n'.replace(
+        /\\n/g,
+        '\n',
+      ),
+    clientEmail: 'firebase-adminsdk-x52bq@civicbase-functions.iam.gserviceaccount.com',
+  }),
+  databaseURL: 'https://civicbase-functions.firebaseio.com',
+  projectId: 'civicbase-functions',
+})
 function fetchSurveyData() {
   // Firestore collection reference
   const surveysCollectionRef = admin.firestore().collection('surveys')
@@ -87,7 +97,6 @@ const formatDate = (date) => new Date(date).toISOString()
 
 // Create a function to wrap a value in quotes if it contains a comma
 const wrapInQuotesIfNeeded = (value) => (value ? (value?.includes(',') ? `"${value}"` : value) : value)
-
 
 function exportSurveyResultsToCsv() {
   const json = require('../export.json')
@@ -274,9 +283,9 @@ function exportSurveyRespondendtsToCsv() {
         wrapInQuotesIfNeeded(respondent?.connection?.autonomous_system_organization),
         respondent?.connection?.autonomous_system_number,
         wrapInQuotesIfNeeded(respondent.region),
-       respondent.postal_code,
+        respondent.postal_code,
         respondent.longitude,
-    respondent.status,
+        respondent.status,
       ]
       surveyRespondendtsCsvData.push(row.join(','))
     })
@@ -286,7 +295,32 @@ function exportSurveyRespondendtsToCsv() {
   fs.writeFileSync('survey_respondents.csv', surveyRespondendtsCsvData.join('\n'))
   console.log('Survey Respondendts CSV export completed successfully!')
 }
+
+function exportSurveyRespondentsNamesToCsv() {
+  const json = require('../export.json')
+  // Define CSV headers
+  const surveyResultsCsvHeaders = ['identifier']
+
+  // Convert JSON data to CSV format
+  const surveyResultsCsvData = [surveyResultsCsvHeaders.join(',')]
+
+  const names = []
+  json.forEach((survey) => {
+    survey?.answers?.forEach((answer) => {
+      if (!names.includes(answer.identifier)) {
+        const row = [answer.identifier]
+        surveyResultsCsvData.push(row.join(','))
+        names.push(answer.identifier)
+      }
+    })
+  })
+
+  // Write CSV data to a file
+  fs.writeFileSync('survey_respondents_names.csv', surveyResultsCsvData.join('\n'))
+  console.log('Survey respondents names CSV export completed successfully!')
+}
 //fetchSurveyData()
-exportSurveyResultsToCsv()
-exportSurveyAccessToCsv()
-exportSurveyRespondendtsToCsv()
+// exportSurveyResultsToCsv()
+// exportSurveyAccessToCsv()
+// exportSurveyRespondendtsToCsv()
+exportSurveyRespondentsNamesToCsv()
--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -3,23 +3,24 @@ import * as firebase from 'firebase/app'
 import * as authentication from 'firebase/auth'
 
 const config = {
-  apiKey: 'AIzaSyB4jCiZBX6-OYiJUBHcuWwXaPXHUWeVQ9s',
+  apiKey: 'AIzaSyA-Go5-5idQJLEkKrLMMiI05GrLdXbhdkY',
   authDomain: 'civicbase-functions.firebaseapp.com',
   projectId: 'civicbase-functions',
   storageBucket: 'civicbase-functions.appspot.com',
   messagingSenderId: '436253743444',
-  appId: '1:436253743444:web:179fa9379371509c5510c1',
+  appId: '1:436253743444:web:6413c855468652795510c1',
+  measurementId: 'G-ER5NGED7PF',
 }
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: 'civicbase-functions',
     privateKey:
-      '-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDYSWiDn0MuDtp8\nAtcMeUzv1xMjaw+f+Oy6VkKc/rzc2kBJ6dZrMXqqeVNttegVa6tDsenB4LUTl/29\nfjYhD1JIBd5HfnEzZw8TfO9QvmGHmdGA7h6VgiGuJcpdybGgWBE+Zw7ZipeUrb8U\nGOyCKxzjba/VMDTv1tuIqrWaGY8ZDskx1trRZ+h+MQ0EUfslfOQduqiqm+Lbf/6S\nJRbwLYW6yGNe4X+NCMkrcVq8nkS2ww6TmgZ1A1AaP29Qym+z8h+dOecMrkF1+1Xc\nA4EtZfE9zeOmCOp+4F7RA/VHp5m9hwGteUyfQiX+ZEP4xdflvnFr4CKzjon/Lg5B\nqiO0jATdAgMBAAECggEABnsWoGEIjov/5HjiUDufQE2Jw4Us++5ly4STrOgeKSD6\nDazt0ZrzMcH+a7poaKHoCRh+6GIPEvX6MrAiN5XhsYpJeYtyoWdsZnlXI2x8+Cb2\nBd68uF6kmzcDLBHD2onhBGVqpr6WIayeHeBMT+8yR6dM/j6aS3h87gitdd9fsBC4\n9s+AmOSPmmW1d9HinMkdWi+N4ZujoOV2GCfStms/VnM980zbuvqZyPZT22XyRLHo\n3S53S41A8W+RS4wp1rKEuf+VQk5IYjR+kkv9e4QGh6lnC+wweXxof0cjDlW39fTs\n/lUEflUugT7akZA3bEZlDCcMxX3WDeyzJF2CvJIiAQKBgQD9vua1xJzyNSUlCW8Y\nJ6wm3YWxzGh3hi7v2PRweg1ZaG+5I+/IZHDHpKxOxaxp1vzxa4s3mMDtB6Z550Ow\n7lHGDPGfZJDPoqWxwJgfSUu3Jw9qr5ulVohXuEaYnUL0iQbXgawcQEwHczqpaKpk\nk3dlTKv3twqJlFdJg5IFlTZyAQKBgQDaNVA78TupR2itR8lxMe5QSyMQM9BgvJW6\nM1F0JHVln3poyFj9zckV895krf/x7g7ZQhZfRHpg5Z71M6BxB1kNIt4ZMmRWWmgE\nzGEDxK8YZfLA4Whcw1P0waPMyAPP+UxRxI1/Md2ZhJClr+lDN/fPVYURZbRT4Xf0\nFp+NJfea3QKBgHUrD3Fjhm05KRu/DXOD7jRMGg4Nqzd12HqcHdNKKJveY6iD9Zb8\n+i7V1VLSaFi/EzGPTUuw13UcET09Er7CbTQcif3l/BBEtGot2pCn0bSS7sV+rUpd\neLu4tbCG/YHZPDWLBSROdE0EYlQTpoqmpX07ODU8ajs5Kk0h2e2GBwwBAoGAOVnz\n2Z1gyfAr0gdhKbAu68mj0Jldtl86lzLvwK/obypIWQv3X9XbeTqchCe7cpHYM0AM\nc0OjSp5vdhD4pVyCClLUvxigiJXAaypwgTYLs0TwVcOo02C/S0At1h/n6Jykc4VO\nj1TC4puWDQY8L9+g8UyP6e+qQrWq+muLyeRlvL0CgYA53jop+dRJuWRBTdTUDt4y\nO1VmTtOJ+6MXgA6J5fU5P3s81LjpK7KfTb2Wqs0Y5pfFb3AP9wCG4wkOzVWaOY+3\nOsqh7U00PweBOQcEbGII4l0RVnfr31R0zu41aB4WgqcQaLAbNyvp1ssrUX9f+2wb\nEuD7aTYSmaja3pwLvqf1rw==\n-----END PRIVATE KEY-----\n'.replace(
+      '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDGKh+fKWKdmDBl\nI6unEzdgpJ15hE7H4JTJDONT+/RCiltyCmjXpgiSZslhYvFW95vNsom/uSKnlSvJ\nxmmXyAHFo9eeAITJ0R5EdYVyaRZsWv439mb4dd/4ZMNGxygiKkslCBOwjZdCix52\nE48FiiGl1dnyp5d+WO3QTDc4buLwHzbP5F7EKPNp37nRGTCft3anviOhCxzRVUrG\np6L/VuDeGLjKkR0asWJZptrjdMSBczX28rWOQNQ/wjbpHZDFWAkSqNjB2pJVIEVe\n4X7H9UkD5Rs5vrqv8cFyeCfAlCcmj2O+LeB1VHzdfcL5ST+clm6leD++SlvuPji0\nDGqK5eMFAgMBAAECggEAEDHBec4pBMk6+aXFN/2TCS8KvrNR7A9UOWxeaGIIksBT\nbtA49zHWm36sHTnq9sXiq36LhINXUkV98zRnmeoHcwsyHY1CgASHkH95YC5qxBol\n+Mhs0js+PXXTaF8lAX3EiSQBPLlyT61AC26kjoa8vALsMWKbnjPoSdCHGfG97AxY\n9a3sdbtUzLUHY0bSALZ3xyLtkWhMr4wq2mxpn4Y7nm4UM2Iq7mQtGm66p4PZJaSF\n9nHik3VY2/xzzwDRntw9PzkPOiPiX7NCQpoRfpa57xlhoWL90dHTik6QaWV1aMa+\nqs6yTW6UVgJcwcl0qzTnFyYxAZwSW2gVSOSRr5xRtQKBgQDnBwUfcBQeIy95Dbch\ncr1tNxRCdtHNQvaIwigxaY8HK3F7TICyQl7tFkfXUIUFD+HdQZivY5DZMwtEQWa7\nBlfXGE2ZLxLvOGFLnfKVbGcggO96PFRoJ1Br0h67sEJDiMbpPq7DH8t8+mpofIPm\nslPGzdg/HTY4o3Ozxom9FaAuCwKBgQDblbk4v6rAh5kCPM9yjWIZidkx+faPbpdQ\nLkVMl4l3X3+CLRXNtrR4LbApj+4LEPkPTP7QT1fVlplNq7CSeuT2x/i5LsUqUPgg\nJILEbPjeHdTGg2wtOypgB9Ge2NAO5D2S1G+8vvwP3xWhWyiEbZCLLgMq6+E74EPd\nuD229iCtLwKBgDnj0m7MQN5EZtLokuj4vWqukI8PAwdyLiCatkObddGQG0GQK/d/\nFhZGrP3RGWPpVOmCVGYuhs9GMR6LXNikgEJPvrIRz6bN8+Nk0rKKlVTZuihhEa1/\nnHFIF5dkVVZi4aT0pgoS34STozJHcS3PpZp9XiCCi2Gp44Y6y6zrzFV9AoGBALS3\n772tuOlFb/8wbwb3J0Kfedqwv2RifzPlMPGNUfyl4voPAv/QG41BBNff9eizqlcW\nZiyo8D//9uG+qM6VSDV2dNRrkqd2Bg/mHJ1ulg+v+3r5lKFRLVnldXbHN8zZuXdL\nUT7E7Axf7Jxkut7qkWiKkN7VTQSLWS90P46B2HjPAoGBAMxbJpK9EYRaDg02Yjx+\nDBjEUO+VbPaQRACZZWuqmwHNGJcKis5mFHWSbNH5QMAq6b7c19WNw2r1u6HNLkA6\nRjRa7koYAp4j1wM2iezH5gJuau+BML8XAuDwDq8Qk7DVfRMEg6mRbFoxFIQaw+8F\nArJr8o+dojTr7fx6aCQueXym\n-----END PRIVATE KEY-----\n'.replace(
         /\\n/g,
         '\n',
       ),
-    clientEmail: 'firebase-adminsdk-1v5bw@civicbase-functions.iam.gserviceaccount.com',
+    clientEmail: 'firebase-adminsdk-x52bq@civicbase-functions.iam.gserviceaccount.com',
   }),
   databaseURL: 'https://civicbase-functions.firebaseio.com',
   projectId: 'civicbase-functions',
